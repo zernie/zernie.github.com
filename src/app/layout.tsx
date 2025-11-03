38673cb53911b91389a9a684832ec65101994329
zernie <zergetaev@gmail.com>
2025-11-04 01:39:03 +0500

    feat: add Google Tag Manager integration for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 
 import { siteConfig } from "@/shared/config";
 import { SiteFooter } from "@/widgets/footer";
@@ -37,6 +38,23 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className="antialiased">
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WGDTD7X');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WGDTD7X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <SiteHeader />
         <main>{children}</main>
         <SiteFooter />
