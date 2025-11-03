7a3057dc0f833f42df1348af0703a88824f8b8c4
zernie <zergetaev@gmail.com>
2025-11-04 00:06:26 +0500

    fix: remove unnecessary pageExtensions from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["page.tsx", "page.ts", "route.ts", "route.tsx"],
   output: "export",
   images: {
     unoptimized: true,
